cf5cd3717301f12bd9838317c0b96a80ce6ad206
marcosgil90 <marcos.rmgil@gmail.com>
2019-03-27 23:44:18 +0000

    landing page first approach

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../assets/css/static-home-page.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+if (window.location.pathname !== '/') {
+	ReactDOM.render(<App />, document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
